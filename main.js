b8ed188fe0fb2d72a9f137779757e0665f727eb4
Godzilla2023 <91707752+Godzilla2023@users.noreply.github.com>
2023-10-06 21:46:34 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     song1 = loadSound("in-the-forest-2-21402.mp3");
     song2 = loadSound(music.mp3);
@@ -11,6 +16,26 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+console.log("Posenet is Initialized");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = "+leftWristX+"leftWristX = "+leftWristY);
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = "+leftWristX+"rightWristX = "+rightWristY);
+    
+    }
 }
 
 function draw(){
